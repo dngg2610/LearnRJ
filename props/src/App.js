e8f10fee0d45e57462551549ea4256f1263cb897
dngg2610 <dngg2610@gmail.com>
2023-05-27 16:45:33 +0700

    done

---
+++ props/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <div>
+      <h1>{props.headerProp}</h1>
+      <h2>{props.contentProp}</h2>
+    </div>
+  );
+}
+
+App.defaultProps = {
+  headerProp: "Header from props...",
+  contentProp: "Content from props..."
+}
+
+
+
+export default App;
