caa7224f7f448e47705da1696e5a323a122119d9
dngg2610 <dngg2610@gmail.com>
2023-06-30 21:52:49 +0700

    done

--- mm/src/components/HomePage.js
+++ mm/src/components/HomePage.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import '../App.css';
 
 function HomePage(props) {
   const { products } = props;
 
   return (
     <div>
+      
       <h1>Trang chủ</h1>
 
-      <div className="row d-flex align-items-stretch">
+      <div className="row d-flex align-items-stretch mt-4">
         {products.map((product) => (
           <div key={product.id} className="col-md-4 mb-3">
             <div className="card h-100">
-              <img src={product.image} alt="Hình ảnh" className="card-img-top" style={{ width: '100%', height: '350px'}} />
+              <img src={product.image} alt="Hình ảnh" className="card-img-top" style={{ width: '100%', height: '350px' }} />
               <div className="card-body">
                 <h5 className="card-title">{product.name}</h5>
                 <p className="card-text">Giá: {product.price} VNĐ</p>
@@ -21,6 +23,7 @@ function HomePage(props) {
           </div>
         ))}
       </div>
+
     </div>
   );
 }
