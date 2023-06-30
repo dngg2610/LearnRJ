9c7047399e18f722af360c9613cbec1f14a68fa7
dngg2610 <dngg2610@gmail.com>
2023-06-30 22:48:38 +0700

    done

--- mm/src/components/AdminPage.js
+++ mm/src/components/AdminPage.js
@@ -10,6 +10,7 @@ function AdminPage(props) {
     const [priceError, setPriceError] = useState('');
     const [descError, setDescError] = useState('');
     const [imageError, setImageError] = useState('');
+    const [editingIndex, setEditingIndex] = useState(-1);
 
     const handleNameChange = (event) => {
         setName(event.target.value);
@@ -17,7 +18,7 @@ function AdminPage(props) {
 
     const handleDescChange = (event) => {
         setDesc(event.target.value);
-    }
+    };
 
     const handlePriceChange = (event) => {
         setPrice(event.target.value);
@@ -45,16 +46,23 @@ function AdminPage(props) {
             return;
         }
 
-
         if (!/^\d+(\.\d{1,2})?$/.test(price)) {
             setPriceError('Vui lòng nhập giá hợp lệ');
             return;
         }
+
         if (!selectedImage) {
             setImageError('Vui lòng chọn hình ảnh');
             return;
         }
-        const newProduct = { name, price, desc, image: URL.createObjectURL(selectedImage) };
+
+        const newProduct = {
+            name,
+            price,
+            desc,
+            image: URL.createObjectURL(selectedImage),
+        };
+
         setProducts([...products, newProduct]);
         setName('');
         setPrice('');
@@ -62,13 +70,67 @@ function AdminPage(props) {
         setNameError('');
         setPriceError('');
         setImageError('');
+    };
 
+    const handleEditProduct = (index) => {
+        const productToEdit = products[index];
+        setName(productToEdit.name);
+        setDesc(productToEdit.desc);
+        setSelectedImage(null);
+        setEditingIndex(index);
+    };
+
+    const handleUpdateProduct = () => {
+        if (!name.trim()) {
+            setNameError('Vui lòng nhập tên sản phẩm');
+            return;
+        }
+
+        if (!desc.trim()) {
+            setDescError('Vui lòng nhập mô tả sản phẩm');
+            return;
+        }
+
+        if (!price.trim()) {
+            setPriceError('Vui lòng nhập giá sản phẩm');
+            return;
+        }
+
+        if (!/^\d+(\.\d{1,2})?$/.test(price)) {
+            setPriceError('Vui lòng nhập giá hợp lệ');
+            return;
+        }
+
+        if (!selectedImage) {
+            setImageError('Vui lòng chọn hình ảnh');
+            return;
+        }
+
+        const updatedProducts = [...products];
+        updatedProducts[editingIndex] = {
+            name,
+            price,
+            desc,
+            image: URL.createObjectURL(selectedImage),
+        };
+
+        setProducts(updatedProducts);
+        setName('');
+        setDesc('');
+        setPrice('');
+        setSelectedImage(null);
+        setImageError('');
+        setEditingIndex(-1);
     };
 
     const handleDeleteProduct = (index) => {
         const updatedProducts = [...products];
         updatedProducts.splice(index, 1);
         setProducts(updatedProducts);
+
+        if (editingIndex === index) {
+            setEditingIndex(-1);
+        }
     };
 
     return (
@@ -78,7 +140,9 @@ function AdminPage(props) {
             <div className="mb-3">
                 <h3>Thêm sản phẩm</h3>
 
-                <label htmlFor="nameInput" className="form-label">Tên sản phẩm:</label>
+                <label htmlFor="nameInput" className="form-label">
+                    Tên sản phẩm:
+                </label>
                 <input
                     type="text"
                     id="nameInput"
@@ -88,17 +152,20 @@ function AdminPage(props) {
                 />
                 {nameError && <div className="invalid-feedback">{nameError}</div>}
 
-                <label htmlFor="descInput" className="form-label">Thêm mô tả:</label>
+                <label htmlFor="descInput" className="form-label">
+                    Thêm mô tả:
+                </label>
                 <input
                     type="text"
                     id="descInput"
                     value={desc}
                     onChange={handleDescChange}
-                    className={`form-control ${descError ? 'is-invalid' : ''}`}
+                    className={`form - control ${descError ? 'is-invalid' : ''}`}
                 />
-                {descError && <div className="invalid-feedback">{descError}</div>}
-
-                <label htmlFor="priceInput" className="form-label">Giá sản phẩm:</label>
+                {descError && <div className="invalid-feedback">{descError}</div>}<br/>
+                <label htmlFor="priceInput" className="form-label">
+                    Giá sản phẩm:
+                </label>
                 <div className="input-group">
                     <input
                         type="text"
@@ -111,7 +178,9 @@ function AdminPage(props) {
                 </div>
                 {priceError && <div className="invalid-feedback">{priceError}</div>}
 
-                <label htmlFor="imageInput" className="form-label">Chọn hình ảnh:</label>
+                <label htmlFor="imageInput" className="form-label">
+                    Chọn hình ảnh:
+                </label>
                 <input
                     type="file"
                     id="imageInput"
@@ -121,23 +190,37 @@ function AdminPage(props) {
                 />
                 {imageError && <div className="invalid-feedback">{imageError}</div>}
 
-                <button onClick={handleAddProduct} className="btn btn-primary mt-3">Thêm</button>
+                <button
+                    onClick={editingIndex === -1 ? handleAddProduct : handleUpdateProduct}
+                    className="btn btn-primary mt-3"
+                >
+                    {editingIndex === -1 ? 'Thêm' : 'Cập nhật'}
+                </button>
             </div>
 
             <h3>Danh sách sản phẩm:</h3>
             {products.map((product, index) => (
-                <div key={index} className="card mb-3 mt-4 " style={{ cursor: "pointer" }}>
-                    <img src={product.image} alt="Hình ảnh" className="card-img-top" style={{ maxWidth: '200px' }} />
+                <div key={index} className="card mb-3 mt-4 " style={{ cursor: 'pointer' }}>
+                    <img
+                        src={product.image}
+                        alt="Hình ảnh"
+                        className="card-img-top"
+                        style={{ maxWidth: '200px' }}
+                    />
                     <div className="card-body">
                         <h5 className="card-title">{product.name}</h5>
                         <p className="card-text">Giá: {product.price} VNĐ</p>
                         <p className="card-text">{product.desc}</p>
-                        <button onClick={() => handleDeleteProduct(index)} className="btn btn-danger">Xóa</button>
+                        <button onClick={() => handleEditProduct(index)} className="btn btn-primary">
+                            Sửa
+                        </button>
+                        <button onClick={() => handleDeleteProduct(index)} className="btn btn-danger">
+                            Xóa
+                        </button>
                     </div>
                 </div>
             ))}
-        </div>
-    );
+        </div>);
 }
 
 export default AdminPage;
